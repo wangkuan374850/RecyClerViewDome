025e42455930f7f116eebddcdb3dfde49cb8d92c
Your Name <375048502@qq.com>
2018-01-11 19:33:34 +0800

    第二次又添加了一行注释，嘿嘿

--- app/src/main/java/yukang/com/dome_5_day/CityBean.java
+++ app/src/main/java/yukang/com/dome_5_day/CityBean.java
@@ -3,7 +3,7 @@ package yukang.com.dome_5_day;
 /**
  * Created by Administrator on 2018/1/11.
  */
-
+//这是第四次提交了哈，我有瞎添了点东西
 public class CityBean extends BaseIndexPinyinBean {
 
     private String city;//城市名字
